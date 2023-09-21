ae83f04f6230e9d1c511ed213218a26fafeb18c3
check-karim <rusakaa7@gmail.com>
2023-09-21 23:19:54 +0200

    listen btn

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<div class="row justify-content-end">
 			<div class="col-md-8 p-5 text-center">
 				<h1 class='p-4  home-text-shadow'>Immerse Yourself in a World of Audio Adventures</h1>
-				<a class='btn btn-small btn-danger' href="login.html">listen now</a>
+				<a class='btn btn-small btn-danger' href="listen.php?page_state=Listen">listen now</a>
 			</div>
 			<br/>
 			<div class="col-12 pt-5 home-text">
